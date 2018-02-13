123ea8e1f8b57ddcc36abb68b29f6acb17fa3e46
umut özel <umutozel@umuts-MacBook-Pro.local>
2018-02-13 11:36:42 +0300

    initial commit

---
+++ CsToTs/CsToTs/TypeDefinition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CsToTs {
+    
+    public class TypeDefinition: InterfaceDefinition {
+
+        public TypeDefinition(
+            string name,
+            string @namespace,
+            TypeDefinition baseType,
+            IEnumerable<MemberDefinition> members,
+            IEnumerable<GenericParameterDefinition> genericParameters = null, 
+            IEnumerable<InterfaceDefinition> implementedInterfaces = null)
+            : base(name, @namespace, members, genericParameters, implementedInterfaces) {
+            
+            Decorators = new List<DecoratorDefinition>();
+        }
+        
+        public bool IsAbstract { get; }
+        public TypeDefinition BaseType { get; }
+        
+        public IList<DecoratorDefinition> Decorators { get; }
+    }
+}
