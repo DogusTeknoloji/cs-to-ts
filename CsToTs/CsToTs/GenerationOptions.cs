f9b34637408bcaed48415fb9a7421beadafe63d7
umut özel <umutozel@umuts-MacBook-Pro.local>
2018-02-14 09:08:27 +0300

    fix folder structure

--- CsToTs/CsToTs/GenerationOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CsToTs {
-    
-    public class GenerationOptions {
-        public bool UseDateForDateTime { get; set; }
-        
-        public static GenerationOptions Default { get; } = new GenerationOptions(); 
-    }
-}
