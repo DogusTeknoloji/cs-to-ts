022c21248636f68caa2b59635ef67846103a51c1
umutozel <umutozel@gmail.com>
2018-03-24 17:17:33 +0300

    Fix build task

--- src/CsToTs/Generator.cs
+++ src/CsToTs/Generator.cs
@@ -4,12 +4,8 @@ namespace CsToTs {
     
     public static class Generator {
 
-        public static string GenerateTypeScript(Type type, TypeScriptOptions options = null) {
-            return GenerateTypeScript(options, type);
-        }
+        public static string GenerateTypeScript(Type type, TypeScriptOptions options = null) => GenerateTypeScript(options, type);
 
-        public static string GenerateTypeScript(TypeScriptOptions options = null, params Type[] types) {
-            return TypeScript.Helper.GenerateTypeScript(types, options ?? TypeScriptOptions.Default);
-        }
+        public static string GenerateTypeScript(TypeScriptOptions options = null, params Type[] types) => TypeScript.Helper.GenerateTypeScript(types, options ?? TypeScriptOptions.Default);
     }
 }
