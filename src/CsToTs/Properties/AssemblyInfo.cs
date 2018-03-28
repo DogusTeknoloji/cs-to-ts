3d373fc930eb801c0aa55090b1699a2428565a40
umutozel <umutozel@gmail.com>
2018-03-28 14:11:37 +0300

    Add assembly info

---
+++ src/CsToTs/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CsToTs - Generate TypeScript from C#")]
+[assembly: AssemblyDescription("CsToTs - Generate TypeScript from C#.")]
+#if DEBUG
+ [assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Doğuş Teknoloji")]
+[assembly: AssemblyProduct("CsToTs - Generate TypeScript from C#")]
+[assembly: AssemblyCopyright("Copyright (c) 2018")]
+[assembly: AssemblyTrademark("CsToTs")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1")]		
+[assembly: AssemblyFileVersion("1")]
